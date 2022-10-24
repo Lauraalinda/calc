cb393921ec61b33782dfadbffa611632287ed187
Nicholas-Kipkoech <nickey968@gmail.com>
2022-10-24 20:54:32 +0300

    fix buttons display values

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
-const inputField=document.getElementById("inputdigit")
+const inputField = document.getElementById("inputdigit")
 const form = document.querySelector('.form')
 const buttons = Array.from(document.querySelectorAll('button'))
 
 
-buttons.forEach(function(element){
-element.addEventListener('click',function(){
-    const tipRate = Number(element.id)
-    console.log( tipRate )
-    
-    
-    // console.log(`Button of id:${element.id} has been clicked`)
+// buttons.forEach(function (element) {
+//     element.addEventListener('click', function () {
+//         let tipRate = Number(element.id)
 
-   })
-})
 
-const buttonIds = buttons.map(function(element){
+
+//     })
+// })
+
+
+for (let button of buttons) {
+    button.addEventListener('click', () => {
+        console.log(button.id)
+
+    })
+
+
+}
+
+
+
+
+
+const buttonIds = buttons.map(function (element) {
     return element.id
 })
-// const button =document.querySelector('button')
-// button. addEventListener('click', function(){
-//   console.log("Button is clicked")
-//  
-// })
-const tr= tipRate
+
 const totalBill = Number(inputField.value)
-function calculateTotalBill(totalBill){
-      console.log(totalBill*tr)
-//    console.log(tipRate*totalBill)
+function calculateTotalBill(totalBill) {
+    console.log(totalBill * tr)
+    //    console.log(tipRate*totalBill)
 }
-form.addEventListener('submit',function(e){
-  e.preventDefault();
-  
-  if (inputField.value === ''){
-      alert("You need to add the bill amount")
-  }else{
-   
-      console.log(calculateTotalBill(Number(inputField.value),tipRate))
-  }
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+
 })
